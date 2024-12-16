d9ce3b5e84832c4f1dd42e3c8fd82cb729ade161
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-12-16 22:05:59 +0700

    feat: add export excel

--- resources/views/template/admin/header.blade.php
+++ resources/views/template/admin/header.blade.php
@@ -21,6 +21,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.3/xlsx.full.min.js"></script>
     <style>
        body {
     overflow: visible;
